1d74fc9f6338ab4bb9239454f826bec90a018e56
JaviRomero00 <javier.romero.perez@iesdonana.org>
2023-03-24 13:39:25 +0100

    agenda

--- guardar.php
+++ guardar.php
@@ -1,13 +1,16 @@
 <?php
+        require 'conexion.php';
         $nombre   = $_POST['nombre'];
         $telefono = $_POST['telefono'];
 
         // Insertar el contacto en la base de datos
         $query = "INSERT INTO contactos (nombre, telefono)
-                       VALUES ('$nombre', '$telefono')";
+                       VALUES ('$nombre', '$telefono') RETURNING id;";
+
         $res = pg_query($con, $query);
 
-        if (pg_num_rows($res) !=0) {
+        if (pg_num_rows($res) != 0) {
+            $fila = pg_fetch_array($res, 0);
             echo "Contacto guardado correctamente";
         } else {
             echo "Error al guardar el contacto";
